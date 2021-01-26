915e6759f49e7cf567c5d73def330108b3c9984d
catiaiazevedo <catiaiazevedo@ua.pt>
2021-01-26 12:37:15 +0000

    restaurantPage

---
+++ app/src/main/java/com/example/zerowaste/RestaurantPage.java
@@ -0,0 +1,18 @@
+package com.example.zerowaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class RestaurantPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_page2);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content3_frame, new RestaurantPageFragment()).commit();
+    }
+}
